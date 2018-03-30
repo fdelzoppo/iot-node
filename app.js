818feeeea2f08152ee7648891959cef414ce72b1
ThaNico <ThaNico@users.noreply.github.com>
2018-03-30 11:47:12 +0200

    leapmotion logs

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var Cylon = require('cylon');
 
+var antispam1=0,antispam2=0,antispam3=0;
 Cylon.robot({
   connections: {
-    leapmotion: { adaptor: 'leapmotion', host: '192.168.140.83' }
+    leapmotion: { 
+		adaptor: 'leapmotion'
+		//,host: '192.168.141.33' 
+	}
+	
   },
 
   devices: {
@@ -10,8 +15,22 @@ Cylon.robot({
   },
 
   work: function(my) {
-    my.leapmotion.on('hand', function(payload) {
-      console.log(payload.toString());
+	my.leapmotion.on('hand', function(payload) {
+		if(antispam1 % 40 == 0) 
+			console.log(payload.toString());
+		antispam1++;
+	});
+	
+	my.leapmotion.on('gesture', function(gesture) {
+		if(antispam2 % 20 == 0) 
+			console.log(gesture.toString());
+		antispam2++;
+    });
+	
+	my.leapmotion.on('frame', function(frame) {
+		if(antispam3 % 60 == 0) 
+			console.log(frame.toString());
+		antispam3++;
     });
   }
 }).start();
