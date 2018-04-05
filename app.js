4340440536ce524f73dae7bca5e5989eaba9b7b6
fdelzoppo <33114128+fdelzoppo@users.noreply.github.com>
2018-04-05 10:07:59 +0200

    app leap

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var Cylon = require('cylon');
 
-var antispam1=0,antispam2=0,antispam3=0;
+"use strict";
+
+var Cylon = require("cylon");
+
+var TURN_TRESHOLD = 0.2,
+    TURN_SPEED_FACTOR = 2.0;
+
+var DIRECTION_THRESHOLD = 0.25,
+    DIRECTION_SPEED_FACTOR = 0.05;
+
+var UP_CONTROL_THRESHOLD = 50,
+    UP_SPEED_FACTOR = 0.01,
+    CIRCLE_THRESHOLD = 1.5;
+
+var handStartPosition = [],
+    handStartDirection = [];
+
+var handWasClosedInLastFrame = false;
+
 Cylon.robot({
   connections: {
     leapmotion: { 
@@ -16,21 +34,57 @@ Cylon.robot({
 
   work: function(my) {
 	my.leapmotion.on('hand', function(payload) {
-		if(antispam1 % 40 == 0) 
-			console.log(payload.toString());
-		antispam1++;
+		
+			//console.log(payload.toString());
+		
 	});
 	
-	my.leapmotion.on('gesture', function(gesture) {
-		if(antispam2 % 20 == 0) 
-			console.log(gesture.toString());
-		antispam2++;
+ my.leapmotion.on('gesture', function(gesture) {
+
+   
+		//On rentre si on fait plus de 0.5 cercle et que l'on stop (en vue)
+        if (gesture.type=='circle' && gesture.state=='stop' && gesture.progress > 0.5 ){
+		//sens horaire => selection
+            if (gesture.normal[2] < 0) {
+                console.log("b"+gesture.toString());
+            };
+		//sens anti horaire => retour
+            if (gesture.normal[2] > 0) {
+                console.log("a"+gesture.toString());
+            }
+        }
+		//Si swipe 
+        if (gesture.type=='swipe' && gesture.state=='stop' ){
+		
+		
+		//Position[0] => axe x (Longueur LeapMotion)
+		//Position[1] => axe z (Hauteur  LeapMotion)
+		//Position[2] => axe y (Largeur du LeapMotion)
+		
+		
+		//Pour swipe droit startposition[x] - position[x]
+		 console.log(gesture);
+		
+          if(gesture.position[0] - gesture.startPosition[0] > 0)
+		  {
+		  console.log("droite");
+		  
+		  }
+		  
+		  if(gesture.position[0] - gesture.startPosition[0] < 0)
+		  {
+		  console.log("gauche");
+		  }
+        }
+            
+        
+		
     });
 	
 	my.leapmotion.on('frame', function(frame) {
-		if(antispam3 % 60 == 0) 
-			console.log(frame.toString());
-		antispam3++;
+		
+			//console.log(frame.toString());
+		
     });
   }
 }).start();
